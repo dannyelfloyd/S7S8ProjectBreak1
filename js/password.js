5667ebb15c43dc4183550b43306bc79b2a984374
dannyelfloyd <dannyelfloyd@gmail.com>
2025-04-09 09:45:43 +0200

    JS Password generaator works

--- js/password.js
+++ js/password.js
@@ -1,1 +1,38 @@
-console.log('HTML & JS conected');
+const inputLength = document.getElementById("inputLength");
+const btnGenerate = document.getElementById("btnGenerate");
+const result = document.getElementById("result");
+
+function generatePasssword(pwchars, length) {
+    const limit = 256 - (256 % pwchars.length);
+    
+    
+    let password = "";
+    let randval;
+    
+    for(let i = 0; i < length; i++) {
+        do {
+            randval = window.crypto.getRandomValues(new Uint8Array(1))[0];
+        } while (randval >= limit);
+        
+        password += pwchars[randval % pwchars.length];
+    };
+    
+    return password;
+};
+
+function genarate() {
+    let length = parseInt(inputLength.value);
+    
+    const pwchars  = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "!@#$%^&*()-_=+";
+
+    console.log(generatePasssword(pwchars, length));
+    result.innerText = generatePasssword(pwchars, length); 
+};
+btnGenerate.addEventListener("click", () => {
+    genarate(); 
+});
