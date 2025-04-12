f47f5fccb84ede2aa0a4bbe65737d11d90d6d257
dannyelfloyd <dannyelfloyd@gmail.com>
2025-04-12 21:10:18 +0200

    CSS Weather Page improvements

--- js/weather.js
+++ js/weather.js
@@ -33,14 +33,52 @@ function getWeather(data) {
     console.log(name,country);
     const {condition: {text, icon}, temp_c, humidity, wind_kph, precip_mm} = data.current;
 
-    weatherDiv.innerHTML = `
-    <h2>${name} / ${country}</h2>
-    <p>${text}</p>
-    <img src="${icon}" alt="${icon}"> 
-    <p>${temp_c}ºC</p>
-    <p>Humedad: ${humidity}%</p>
-    <p>Viento: ${wind_kph}km/h</p>
-    <p>Precipitaciones: ${precip_mm} mm</p>
+    weatherDiv.innerHTML += `
+    <div class="location-date-container">
+      <div class="location">
+        <span class="material-symbols-rounded">
+          location_on
+        </span>
+        <h4 class="country-txt">${name}, ${country}</h4>
+      </div>
+      <h5 class="current-date-tx">We, 07 Aug</h5>
+    </div>
+    <div class="weather-summary-container">
+      <img src="${icon}" alt="${icon}">
+      <div class="weather-summary-info">
+        <h3 class="temp-txt">${temp_c}ºC</h3>
+        <h4 class="condition-txt">${text}</h4>
+      </div>
+    </div>
+    <div class="weather-conditions-container">
+      <div class="condition-item">
+        <span class="material-symbols-rounded">
+          humidity_percentage
+        </span>
+        <div class="condition-info">
+          <h5>Humidity</h5>
+          <h5>${humidity}%</h5>
+        </div>
+      </div>
+      <div class="condition-item">
+        <span class="material-symbols-rounded">
+          air
+        </span>
+        <div class="condition-info">
+          <h5>Wind</h5>
+          <h5>${wind_kph} Km/h</h5>
+        </div>
+      </div>
+      <div class="condition-item">
+        <span class="material-symbols-rounded">
+          water_drop
+        </span>
+        <div class="condition-info">
+          <h5>Rain</h5>
+          <h5>${precip_mm} mm</h5>
+        </div>
+      </div>
+    </div>
     `;
 };
 
@@ -53,11 +91,11 @@ function getForestcast(data) {
         const hour = time.split(" ")[1];
         console.log('time.split(" ")[1]',hour);
         forestcastDiv.innerHTML += `
-        <li>
-            <p>${hour}<p>
-            <img class="weather-icon" src="http:${icon}" alt="">
-            <p>${temp_c} °C</p>
-        </li>
+        <div class="forestcast-item">
+          <h5 class="forestcast-item-hour">${hour}</h5>
+          <img src="${icon}" alt="${icon}">
+          <h5 class="forestcast-item-temp">${temp_c}ºC</h5>
+        </div>
         `
     });
 
