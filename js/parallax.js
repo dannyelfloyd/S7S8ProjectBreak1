6540a55ba8d5acd6c216ba9ce392bbd7013a4db8
dannyelfloyd <dannyelfloyd@gmail.com>
2025-04-16 18:56:07 +0200

    Index Style adjust

--- js/parallax.js
+++ js/parallax.js
@@ -1,17 +1,24 @@
 const title = document.querySelector('#title');
-const Layer1Front = document.querySelector('#Layer1Front');
+const layer1Front = document.querySelector('#layer1Front');
 const layer2Right = document.querySelector('#layer2Right');
 const layer2Left = document.querySelector('#layer2Left');
 const layer3 = document.querySelector('#layer3');
 
 window.addEventListener('scroll', () => {
     let scroll = window.scrollY;
+    console.log(scroll);
 
     layer2Left.style.right = scroll + 'px';
     layer2Right.style.left = scroll + 'px';
     layer3.style.top = scroll * 2.5 + 'px';
-    Layer1Front.style.top = scroll * 1 + 'px';
     title.style.bottom = scroll +'px';
 
+    layer1Front.style.top = scroll * 1 + 'vh';
+    /*if (scroll >= 70) {
+        layer1Front.style.top = scroll * 1 + 'vh';
+        
+    } else {
+        layer1Front.style.top = '70vh';
+    };*/
 
 });
